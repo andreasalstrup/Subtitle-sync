5851492f429a9e2ff6cf1a43efb4bccc6e8cbd3d
Andreas Knudsen Alstrup <andreas090994@gmail.com>
2022-07-20 23:56:02 +0200

    subtitle.vue fieldset

--- src/components/Subtitle.vue
+++ src/components/Subtitle.vue
@@ -1,6 +1,6 @@
 <template>
-    <div id="sub-container" class="inner-container" :style="downloadReady ? 'opacity: 1' : 'opacity 0.5'" :key="group[0][0]" v-for="group in subtitle">
-        <h3>{{ group[0] }}</h3>
+    <fieldset id="sub-container" class="inner-container" :style="downloadReady ? 'opacity: 1' : 'opacity 0.5'" :key="group[0][0]" v-for="group in subtitle">
+        <legend>{{ group[0] }}</legend>
         <h3>Start Time</h3>
             <input 
                 :id="`start-${group[0][0]}`" 
@@ -16,7 +16,7 @@
                 :value="group[3]"
                 :style="downloadReady ? '' : 'pointer-events:none;'">
         <h3>{{ group[4] }}</h3>
-    </div>
+    </fieldset>
 </template>
 
 <script>
